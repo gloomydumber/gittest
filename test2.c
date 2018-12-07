7445bce9e07f352cebe6209b60173e6393c0cfe9
강동민 <38835104+gloomydumber@users.noreply.github.com>
2018-12-07 13:55:28 +0900

    3중 malloc

--- test2.c
+++ test2.c
@@ -1,93 +1,60 @@
-#include <stdio.h> // version for VLA(Variable-Length-Array) C99 Standards.. not for visual studio(c89)
-#include <stdlib.h> // for rand() function
+// i made *** malloc !!!!!!!
 
-void setsize(int *x, int *y);
-void alloc(int x, int y, int (*arr)[y]);
-void printarr(int x,int y,int (*arr)[y]);
-void checking(int x,int y,int (*arr)[y]);
-int findmax(int x,int y,int (*arr)[y]);
-int findmin(int x,int y,int (*arr)[y]);
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-	int x; int y;
-	int max, min;
-	setsize(&x,&y);
-	int arr[x][y];
-	alloc(x,y,arr);
-	printarr(x,y,arr);
-	checking(x,y,arr);
-	printarr(x,y,arr);
-	max=findmax(x,y,arr);
-	min=findmin(x,y,arr);
-	printf("max : %2d\nmin : %2d\n",max,min);
-}
+	int i,j,k;
+	int high;
+	int heng;
+	int yul;
+	
+	printf("type high : ");
+	scanf("%d",&high);
+	printf("type heng : ");
+	scanf("%d",&heng);
+	printf("type yul : ");
+	scanf("%d",&yul);
+	
+	int ***a=(int***)malloc(sizeof(int**)*high);
 
-void setsize(int *x, int *y)
-{
-	printf("type x : ");
-	scanf("%d",x);
-	printf("type y : ");
-	scanf("%d",y);
-}
-
-void alloc(int x,int y,int (*arr)[y])
-{
-	int i=0, j=0;
-	for(i=0;i<x;i++)
-		for(j=0;j<y;j++)
-			arr[i][j]=rand()%100;
-}
-
-void printarr(int x,int y,int (*arr)[y])
-{
-	for(int i=0;i<x;i++)
+	for(i=0;i<high;i++)
 	{
-		printf("{ ");
-		for(int j=0;j<y;j++)
-		{
-			printf("%2d ",arr[i][j]);
-		}
-		printf("}");
-		printf("\n");
+		a[i]=(int**)malloc(sizeof(int*)*heng);
 	}
-}
-void checking(int x,int y,int (*arr)[y])
-{
-	int i,j;
-		for(i=0;i<x;i++)
+	
+	for(i=0;i<high;i++)
+	{
+		for(j=0;j<heng;j++)
 		{
-			for(j=0;j<y;j++)
-			{
-				while(arr[i][j]==0)
+		a[i][j]=(int*)malloc(sizeof(int)*yul);
+		}
+
+}
+
+for(i=0;i<high;i++)
+	{
+		for(j=0;j<heng;j++)
+		{
+			for(k=0;k<yul;k++)
 				{
-					arr[i][j]=rand()%100;
-					printf("zero detreced\n");
+					printf("type a[%d][%d][%d] = ",i,j,k);
+					scanf("%d",&*(*(*(a+i)+j)+k));
 				}
 			}
 		}
-}
-
-int findmax(int x,int y,int (*arr)[y])
-{
-	int temp=0;
-	int i=0, j=0;
-
-	for(i=0;i<x;i++)
-		for(j=0;j<y;j++)
-			temp=temp>arr[i][j] ? temp:arr[i][j];
-
-	return temp;
-}
-
-int findmin(int x,int y,int (*arr)[y])
-{
-	int temp=100;
-	int i=0, j=0;
-
-	for(i=0;i<x;i++)
-		for(j=0;j<y;j++)
-			temp=temp<arr[i][j] ? temp:arr[i][j];
-
-	return temp;
+	
+		for(i=0;i<high;i++)
+			{
+				printf("high %d :\n",i);
+				for(j=0;j<heng;j++)
+				{
+					for(k=0;k<yul;k++)
+						{
+							printf("%4d",*(*(*(a+i)+j)+k));
+						}
+						printf("\n");
+					}
+				}
 }
