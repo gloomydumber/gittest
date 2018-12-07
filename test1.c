d10b0fdca502c57d44919bb05b822d92d88eae6a
강동민 <38835104+gloomydumber@users.noreply.github.com>
2018-12-07 15:42:49 +0900

    temporary

--- test1.c
+++ test1.c
@@ -1,17 +1,57 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define N 3
+
+struct students{
+  int snumber;
+  char *name[30];
+  char *addr[30];
+  int age;
+  float score;
+};
+
+void getinfo(struct students *st, int n);
+void printinfo(struct students *st, int n);
 
 int main(void)
 {
-	int a[10]={};
-	int arr[5][5]={};
-	int i,j;
-	for(i=0;i<5;i++)
-	{
-		for(j=0;j<5;j++)
-			printf("%d ",arr[i][j]);
-		printf("\n");
-	}
-	for(i=0;i<10;i++)
-		printf("%d ",a[i]);
-	return 0;
+  int n =N;
+  struct students st[N];
+  getinfo(st, n);
+  printinfo(st, n);
+  return 0;
 }
+
+void getinfo(struct students *st, int n)
+{
+  int i;
+  for(i=0;i<N;i++)
+  {
+    printf("type %d's student number : ",i+1);
+    scanf("%d",&st[i].snumber);
+    printf("type %d's student name : ",i+1);
+    scanf("%s",*(*st[i].name));
+    printf("type %d's student addr : ",i+1);
+    scanf("%s",*(*st[i].addr));
+    printf("type %d's studnet age : ",i+1);
+    scanf("%d",&st[i].age);
+    printf("type %d's student score : ",i+1);
+    scanf("%f",&st[i].score);
+  }
+}
+
+void printinfo(struct students *st, int n)
+{
+  int i;
+  for(i=0;i<N;i++)
+  {
+    printf("%d's student number : %d\n",i+1,st[i].snumber);
+    printf("%d's student name : %s\n",i+1,st[i].name);
+    printf("%d's student addr : %s\n",i+1,st[i].addr);
+    printf("%d's studnet age : %d\n",i+1,st[i].age);
+    printf("%d's student score : %f\n",i+1,st[i].score);
+  }
+}
+// 평균나이
+// 평균학점
